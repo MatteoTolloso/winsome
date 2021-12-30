997843050393263a92cd3e4bde46fef8f5db278a
MatteoTolloso <matteotolloso@gmail.com>
2021-12-30 23:57:52 +0100

    client

--- Winsome Client/src/ClientMain.java
+++ Winsome Client/src/ClientMain.java
@@ -1,4 +1,6 @@
 
+import java.io.*;
+import java.net.Socket;
 import java.rmi.NotBoundException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
@@ -12,12 +14,15 @@ import java.util.Arrays;
 public class ClientMain {
 
     private static int registryPort = 8888;
+    private static String hostname = "127.0.0.1";
+    private static String TER = "\r\n";
 
     
     public static void main(String[] args){
 
         System.out.println("Client avviato");
 
+        /*
         try {
             Registry r = LocateRegistry.getRegistry(registryPort);
             
@@ -31,6 +36,44 @@ public class ClientMain {
 
         } catch (RemoteException | NotBoundException | NullPointerException | UserAlrExiException | InvalidUsernameException e) {
             e.printStackTrace(); 
+        }
+        */
+
+        try (Socket server = new Socket(hostname, 9999);
+            DataOutputStream outToClient = new DataOutputStream(server.getOutputStream());
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(server.getInputStream()))
+            ){
+
+            BufferedReader commandLine = new BufferedReader(new InputStreamReader(System.in));
+            StringBuilder request;
+
+            do{
+               request = new StringBuilder();
+
+                request.append( commandLine.readLine() );
+
+                request.append(TER);
+
+                outToClient.writeBytes(request.toString());
+
+                String response = inFromClient.readLine();
+                
+                while(inFromClient.readLine())
+    
+                System.out.println(response);
+
+                
+            }while (! (request.toString().contains("logout")));
+
+        
+
+
+            server.close();
+
+
+        } catch (IOException e) {
+            
+            e.printStackTrace();
         }
         
     }
